1283720e61af5e8547fa04003839657526f0973b
kmng <kmng@ucla.edu>
2014-07-17 10:49:41 -0700

    add bootstrap

--- app/views/about.php
+++ app/views/about.php
@@ -1,3 +1,7 @@
-
-<h2>About this site</h2>
-There are over <?php echo $number_of_cats; ?> cats on this site!
+@extends('master')
+@section('header')
+  <h2>About this site</h2>
+@stop
+@section('content')
+  <p>There are over {{$number_of_cats}} cats on this site!</p>
+@stop
